ecf5f086832e690efc0b66740caa7fc17f1bf1b1
emilytaylorucf <emilyetaylor1020@knights.ucf.edu>
2021-03-05 13:35:44 -0500

    starting app.js

--- src/App.js
+++ src/App.js
@@ -1,43 +1,98 @@
 import logo from './logo.svg';
 import './App.css';
+import { render } from '@testing-library/react';
+import React from 'react';
 let checklist = [
   {
-    completed: true,
+    completed: false,
     description: "Star Wars: The Empire Strikes Back",
+    genre: "Sci-Fi",
   },
   {
-    completed: true,
+    completed: false,
     description: "The Hobbit",
+    genre: "Fantasy",
   },
   {
-    completed: true,
+    completed: false,
     description: "How to Train Your Dragon",
+    genre: "Fantasy",
   },
   {
-    completed: true,
+    completed: false,
     description: "Ready Player One",
+    genre: "Sci-Fi"
   },
   {
-    completed: true,
+    completed: false,
     description: "Avengers: End Game",
+    genre: "SuperHero",
   },
   {
-    completed: true,
-    description: "Captain America: Civil War"
+    completed: false,
+    description: "Captain America: Civil War",
+    genre: "SuperHero",
   },
 ]
-function App() {
+
+let newList = [
+  {
+    completed: false,
+    description: "Iron Man 3",
+    genre: "SuperHero",
+  },
+  {
+    completed: false,
+    description: "The Lord of the Rings: Return of the King",
+    genre: "Fantasy",
+  },
+  {
+    completed: false,
+    description: "How to Train Your Dragon 3",
+    genre: "Fantasy",
+  },
+  {
+    completed: false,
+    description: "Watchlist is full",
+    genre: "none",
+  },
+]
+
+class App extends React.Component {
+  
+  constructor(props){
+    super(props)
+    this.state = {
+      checklist,
+      newList
+    }
+  }
+  
+  addMovie = () =>{
+    this.setState(prevState =>(
+      {
+        checklist: [...prevState.checklist,
+          {completed: false, description: "IronMan"}]
+      }
+    ))
+  }
+
+  render(){
   return (
     <>
       <h1>My Watchlist</h1>
-      {checklist.map((item) => (
+      {this.state.checklist.map((item) => (
         <div>
-          <input type = "checkbox"></input>
-          <span>{item.description}</span>
+          <label>
+          <input type = "checkbox" defaultChecked = {item.completed}></input>
+          {item.description}
+          </label>
         </div>
       ))}
+      <button onClick = {() =>this.addMovie()}>Add a Movie</button>
     </>
   );
+  }
 }
 
 export default App;
