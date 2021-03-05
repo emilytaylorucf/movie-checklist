8c2cb24b9b4b6bcdc8231153c3fb9839685e6102
emilytaylorucf <emilyetaylor1020@knights.ucf.edu>
2021-03-05 13:01:11 -0500

    creating movies array

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
-
+let checklist = [
+  {
+    completed: true,
+    description: "Star Wars: The Empire Strikes Back",
+  },
+  {
+    completed: true,
+    description: "The Hobbit",
+  },
+  {
+    completed: true,
+    description: "How to Train Your Dragon",
+  },
+  {
+    completed: true,
+    description: "Ready Player One",
+  },
+  {
+    completed: true,
+    description: "Avengers: End Game",
+  },
+  {
+    completed: true,
+    description: "Captain America: Civil War"
+  },
+]
 function App() {
   return (
-   <div>Hello World!</div>
+    <>
+      <h1>My Watchlist</h1>
+      {checklist.map((item) => (
+        <div>
+          <input type = "checkbox"></input>
+          <span>{item.description}</span>
+        </div>
+      ))}
+    </>
   );
 }
 
